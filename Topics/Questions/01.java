ad0ea0686800c57f71615ec2edbb5cc372014c4b
Pratik Sharma <118264213+pratiksh21@users.noreply.github.com>
2024-08-09 15:27:40 +0530

    Create 01.java

---
+++ Topics/Questions/01.java
@@ -0,0 +1,77 @@
+---------------why class does not support multiple inheritance while interfaces does ?
+
+Now, think of class as the blueprint for making these toy cars. Each class tells us what powers (or methods) a toy car has. But in Java, when you make a new toy car, it can only get powers from one blueprint (or class).
+
+------------Step 1: Let's Make a Fast Car!
+We start with a simple car that can go really fast:
+
+java
+Copy code
+class FastCar {
+    void goFast() {
+        System.out.println("Zoom! I'm going super fast!");
+    }
+}
+------------Step 2: Let's Make a Noisy Car!
+Now, let's make another car that can make noise:
+
+java
+Copy code
+class NoisyCar {
+    void makeNoise() {
+        System.out.println("Vroom Vroom! I'm making noise!");
+    }
+}
+------------Step 3: What If We Want a Car That Can Do Both?
+In Java, if we want a car that can go fast and make noise, we can't directly combine both FastCar and NoisyCar to make a new supercar. Instead, we have to choose one class to inherit from. But we can use interfaces to give our car both powers.
+
+Interface Time!
+An interface is like a promise. It says, "Any car that uses me will have this power." Let's make two interfaces:
+
+java
+Copy code
+interface CanGoFast {
+    void goFast();
+}
+
+interface CanMakeNoise {
+    void makeNoise();
+}
+------------Step 4: Build a Supercar That Does Both!
+Now, we’ll make a supercar that promises to go fast and make noise:
+
+java
+Copy code
+class SuperCar implements CanGoFast, CanMakeNoise {
+    public void goFast() {
+        System.out.println("Zoom! I'm going super fast!");
+    }
+
+    public void makeNoise() {
+        System.out.println("Vroom Vroom! I'm making noise!");
+    }
+}
+------------Step 5: Play With Your Supercar!
+Now, let's play with the SuperCar we just built:
+
+java
+Copy code
+public class Main {
+    public static void main(String[] args) {
+        SuperCar mySuperCar = new SuperCar();
+        mySuperCar.goFast();     // Output: Zoom! I'm going super fast!
+        mySuperCar.makeNoise();  // Output: Vroom Vroom! I'm making noise!
+    }
+}
+--------Summary:
+
+Classes are like blueprints for toy cars, but you can only use one at a time.
+Interfaces are like promises that say, "This car will have this power."
+Java uses interfaces to give cars (or classes) more than one power.
+Just like building and playing with toy cars, programming in Java is all about building things step by step!
+
+
+
+
+
+
